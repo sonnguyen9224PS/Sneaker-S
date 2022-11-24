2da51790a7c388348a34925a65af32f770d05543
sonnguyen9224PS <sonnguyenlj@gmail.com>
2022-11-24 16:25:40 +0700

    amen

--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -98,6 +98,7 @@ const RegisterPage = () => {
                       },
                       {
                         type: "email",
+                        message: "Không phải là email!",
                       },
                     ]}
                   >
